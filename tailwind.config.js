9ee382af39133ddd9bb063460f663f1f2db71576
thang11103 <thangutc11103@gmail.com>
2023-12-20 14:47:36 +0700

    HL

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'nunito': ['Nunito Sans', 'sans-serif'],
+      'prompt': ['Prompt', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
